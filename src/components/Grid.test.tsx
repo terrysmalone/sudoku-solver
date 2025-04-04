f431f84cc07f73c2afce1c01a4b3cf3fb74456b8
Terry Malone <terrysmalone@hotmail.com>
2025-04-04 21:17:52 +0100

    Add some breaking tests

---
+++ src/components/Grid.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+
+test("Empty squares should render", () => {
+  const squares: (number | undefined)[] = new Array(9).fill(undefined);
+  render(<Grid squares={squares}></Grid>);
+
+  const items = screen.getAllByTestId("grid-item");
+
+  expect(items).toHaveLength(9);
+
+  for (const item of items) {
+    const test = item.textContent;
+    expect(!isNaN(Number(test))).toBe(true);
+  }
+});
+
+test("numbered squares should render", () => {
+  const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  render(<Grid squares={squares}></Grid>);
+
+  const items = screen.getAllByTestId("grid-item");
+
+  expect(items).toHaveLength(9);
+
+  let counter = 0;
+  for (const item of items) {
+    const test = item.textContent;
+    expect(Number(test)).toEqual(squares[counter]);
+    counter++;
+  }
+});

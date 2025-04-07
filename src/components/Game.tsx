a72d92c725dc00d8cc6c653d2ca2a526ef83c30b
Terry Malone <terrysmalone@hotmail.com>
2025-04-07 22:10:15 +0100

    Generate different test boards

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,44 +1,34 @@
 import React, { useState } from "react";
 import { Board } from "./Board";
-import { populateTestGrid } from "../utils/generate";
+import { countUp } from "../utils/generate";
+import "../Styles/Game.css";
 
 export const Game = () => {
-  const [grids, setGrids] = useState<(number | undefined)[][]>([
-    [1, 2, undefined, 4, 5, 6, 7, 8, 9],
-    [4, undefined, 6, 7, undefined, undefined, 1, 2, 3],
-    [7, 8, 9, 1, 2, 3, 4, 5, 6],
-    populateTestGrid(2),
-    populateTestGrid(5),
-    populateTestGrid(8),
-    populateTestGrid(3),
-    populateTestGrid(6),
-    [1, 1, undefined, 1, 1, 1, 1, 1, 1],
-    // populateTestGrid(9),
-  ]);
+  const [grids, setGrids] = useState<(number | undefined)[][]>(countUp());
 
   const handleClick = (gridIndex: number, squareIndex: number) => {
-    console.log("gridIndex: ", gridIndex);
-    console.log("squareIndex: ", squareIndex);
-
     const newGrids = [...grids];
 
-    const val = grids[gridIndex][squareIndex];
-    console.log("val: ", val);
+    newGrids[gridIndex][squareIndex] = getNextValue(
+      grids[gridIndex][squareIndex],
+    );
 
-    if (val === undefined) {
-      newGrids[gridIndex][squareIndex] = 1;
-    } else if (val === 9) {
-      newGrids[gridIndex][squareIndex] = undefined;
-    } else {
-      // @ts-ignore
-      newGrids[gridIndex][squareIndex]++;
-    }
     setGrids(newGrids);
   };
 
   return (
-    <div>
+    <div className="game">
       <Board grids={grids} onSquareClick={handleClick} />
     </div>
   );
 };
+
+function getNextValue(num: number | undefined): number | undefined {
+  if (num === undefined) {
+    return 1;
+  } else if (num === 9) {
+    return undefined;
+  } else {
+    return num + 1;
+  }
+}
--- src/utils/generate.tsx
+++ src/utils/generate.tsx
@@ -1,6 +1,20 @@
-export const populateTestGrid = (
-  startNumber: number,
-): (number | undefined)[] => {
+export const testFullyPopulated = (): (number | undefined)[][] => {
+  let grid: (number | undefined)[][] = [
+    populateTestGrid(1),
+    populateTestGrid(4),
+    populateTestGrid(7),
+    populateTestGrid(2),
+    populateTestGrid(5),
+    populateTestGrid(8),
+    populateTestGrid(3),
+    populateTestGrid(6),
+    populateTestGrid(9),
+  ];
+
+  return grid;
+};
+
+const populateTestGrid = (startNumber: number): (number | undefined)[] => {
   let squares: (number | undefined)[] = [];
 
   for (let i = 0; i < 9; i++) {
@@ -13,4 +27,33 @@ export const populateTestGrid = (
   }
 
   return squares;
+};
+
+export const testVeryEasy = (): (number | undefined)[][] => {
+  let grid: (number | undefined)[][] = [
+    [9, 8, 6, 7, undefined, 2, 5, 4, 3],
+    [3, 2, 4, 5, 9, 8, 6, 7, undefined],
+    [undefined, 7, 8, 3, 4, 6, 9, 5, 2],
+    [3, 2, 7, undefined, 5, 8, 4, 6, 9],
+    [undefined, 5, 6, 9, 4, 2, 7, 8, 3],
+    [4, 9, 8, 6, 3, 7, 5, 2, undefined],
+    [6, 9, undefined, 2, 7, 5, 8, 3, 4],
+    [8, 3, 5, 4, undefined, 9, 2, 6, 7],
+    [2, 9, 4, 8, 6, 3, 7, undefined, 5],
+  ];
+
+  return grid;
+};
+
+export const countUp = (): (number | undefined)[][] => {
+  let grid: (number | undefined)[][] = [];
+
+  for (let i = 0; i < 9; i++) {
+    grid[i] = [];
+    for (let j = 0; j < 9; j++) {
+      grid[i][j] = i * 9 + j;
+    }
+  }
+
+  return grid;
 };
